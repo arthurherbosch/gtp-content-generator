47f373995f69b7433b1705b0e1e11da575e7d1b9
arthurherbosch <45171438+arthurherbosch@users.noreply.github.com>
2023-05-11 15:53:42 +0200

    d

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if check_password():
     if type == 'Video Script':
         brief = st.text_area("Brief", placeholder="A video on a trend that's cropping up on the newswires - matching training to the time of your cycle. \n The US women’s soccer team coach partly attributes their 2019 World Cup win to cycle synching, and UK club Chelsea (which has Matildas skipper Sam Kerr on the team) tailor all their training to the players’ periods.  \n Content to mention that you don't have to be an athlete to benefit from cycle synching ", help="Make sure to provide a detailed brief that includes all the information needed to create a quality scripts. You can put in articles for reference or put in sources. Tell the script what the focus should be, this will create better results. **The better the brief, the better the script**")
         articles = st.text_area("Sources", placeholder="Link articles here. Put a link on every new line. \n\n https://www.example.com/ \n https://www.example.com/ ")
-        articles = articles.split('\n')
+        articles_list = articles.split('\n')
         
         article_string = ""
         for index , article in articles:
