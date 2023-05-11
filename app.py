e71d51d12bab4f0d1263f5b52e34e49c1e0ae6cd
arthurherbosch <45171438+arthurherbosch@users.noreply.github.com>
2023-05-11 16:50:36 +0200

    d

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if check_password():
                 article_string = ""
                 counter = 1
                 for article in articles_list:
-                    result  = get_article(article)
+                    #result  = get_article(article)
                     article_string += f"Article %s: \n{article} \n\n ## \n\n" % (counter) 
                     counter += 1
                 end_prompt = f"Create a video script for a {video_len}-seconds {type_vid}. \n \nTopic: {video_title} \n\n Brief: {brief} \n\n\n You can use these articles/texts:\n{article_string} "
