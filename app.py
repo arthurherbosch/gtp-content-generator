fc67b472e7f333b89ca05fd929ec54b09b75a6a6
arthurherbosch <45171438+arthurherbosch@users.noreply.github.com>
2023-06-26 15:07:47 +0100

    sss

--- app.py
+++ app.py
@@ -130,8 +130,7 @@ if check_password():
     
 
         if st.button("Clear", key="clear"):
-            st.session_state["script_messages"] = LLH_SCRIPT[:5]
-            
+            st.session_state["script_messages"] = LLH_SCRIPT[:11]
         for i in range(len(st.session_state["script_messages"])-1, 6, -1):
             if st.session_state["script_messages"][i]['role'] == 'user':
                 message(st.session_state["script_messages"][i]['content'], is_user=True)
