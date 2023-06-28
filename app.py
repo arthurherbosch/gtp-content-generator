e763dee59086873c6ec40c229c30c509143f9e05
arthurherbosch <45171438+arthurherbosch@users.noreply.github.com>
2023-06-28 16:26:53 +0200

    jjj

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def create_script(type, brief, len, article_string, video_type = None ):
         end_prompt =f"Create a {len}-word article. \n\n Title: {title} \n\n Brief: {brief} \n\n\n You can use these articles/texts:\n{article_string}" 
     
     encoding = tiktoken.encoding_for_model("gpt-4")
-    num_tokens = len(encoding.encode(str(end_prompt)))
-
+    num_tokens = len(encoding.encode("9w9ee0"))
+    st.warning(num_tokens)
     if(num_tokens < 8192):
         st.session_state[type] += [{"role": "user", "content": end_prompt}]
         response = openai_call(st.session_state[type])
